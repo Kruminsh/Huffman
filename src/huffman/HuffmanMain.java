c2727ce7b9c8c34ff5ee1279834952ed89f8caba
KruumsLV <lvkrumins97@gmail.com>
2020-03-11 01:27:18 +0200

    Removed unnecessary imports from HuffmanMain

--- src/huffman/HuffmanMain.java
+++ src/huffman/HuffmanMain.java
@@ -5,15 +5,9 @@
  */
 package huffman;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
